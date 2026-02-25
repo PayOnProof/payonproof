30757798dea1624e7edce4e4316eccc3cb6398d7
Kenneth Martínez Vargas <120770780+kennethma08@users.noreply.github.com>
2026-02-25 11:29:14 -0600

    Delete m_page directory

--- m_page/components/gradient-mesh.tsx
+++
@@ -1,7 +0,0 @@
-export function GradientMesh() {
-  return (
-    <div className="gradient-mesh" aria-hidden="true">
-      <div className="mesh-orb" />
-    </div>
-  );
-}
--- m_page/lib/fxRates.ts
+++
@@ -1,7 +0,0 @@
-import { Currency } from "./types"
-export const fxRates: Record<Currency, number> = {
-  USD: 1,
-  CRC: 520,
-  MXN: 17,
-  COP: 4000
-}
